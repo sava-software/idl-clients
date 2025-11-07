4580be4e1e0d99c520a26352cf02e6f2a6aee1ca
jpe7s <jpe7s.salt188@passfwd.com>
2025-11-07 07:57:20 -0600

    Publish all modules with the same version.

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -2,15 +2,12 @@ plugins {
   id("software.sava.build.feature.publish-maven-central")
 }
 
-val selectedModule: String? = providers
-  .gradleProperty("nmcp.publish.project")
-  .orElse(providers.environmentVariable("NMCP_PUBLISH_PROJECT"))
-  .orNull
-
 dependencies {
-  if (selectedModule != null) {
-    nmcpAggregation(project(selectedModule.takeIf { it.startsWith(":") } ?: ":idl-clients-$selectedModule"))
-  } else {
-    nmcpAggregation(project(":idl-clients-core"))
-  }
+  nmcpAggregation(project(":idl-clients-cctp"))
+  nmcpAggregation(project(":idl-clients-core"))
+  nmcpAggregation(project(":idl-clients-drift"))
+  nmcpAggregation(project(":idl-clients-jupiter"))
+  nmcpAggregation(project(":idl-clients-kamino"))
+  nmcpAggregation(project(":idl-clients-oracles"))
+  nmcpAggregation(project(":idl-clients-spl"))
 }
